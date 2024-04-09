ebf27081d28761c10335167e5bfd89df92e0b9c5
PeteNaJaXD <91317409+PeteNaJaXD@users.noreply.github.com>
2024-04-09 19:11:46 +0000

    dwasdfawdsaw

--- VJFQp3NRIlXmXtXYVcgFIvYNbs.lua
+++ VJFQp3NRIlXmXtXYVcgFIvYNbs.lua
@@ -366,11 +366,10 @@ task.spawn(function()
 					local CurrentField = getgenv().Script_Setting['Selected_Field']
 					local target : BasePart = GetTarget(CurrentField)
 					repeat task.wait()
-						print(Magnitude(target.Position))
 						Tween(target.CFrame, true)
 						Character().Humanoid.WalkSpeed = getgenv().Script_Setting['Walk_Speed']
 						game:GetService("ReplicatedStorage").Events.ToolCollect:FireServer()
-					until not getgenv().Script_Setting['Auto_Farm'] or target.Parent == nil or target.Transparency >= 1 or Magnitude(target.Position) <= 7 or not target.Parent or not target or Check_Capacity() >= 100 or CurrentField ~= getgenv().Script_Setting['Selected_Field']
+					until not getgenv().Script_Setting['Auto_Farm'] or target.Parent == nil or target.Transparency >= 1 or Magnitude(target.Position) <= 8 or not target.Parent or not target or Check_Capacity() >= 100 or CurrentField ~= getgenv().Script_Setting['Selected_Field']
 					StopTween(getgenv().Script_Setting['Auto_Farm'])
 				end
 			end
